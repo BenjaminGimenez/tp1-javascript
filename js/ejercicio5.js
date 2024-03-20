5239fdac4e93bae709b760f175084a79a621d979
Benjamin Gimenez <benjagimenez824@gmail.com>
2024-03-20 20:22:15 -0300

    problema 5 resuelto usuando parseInt

---
+++ js/ejercicio5.js
@@ -0,0 +1,6 @@
+//5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+
+let num1 = parseInt(prompt('ingrese el primer numero para sumar'))
+let num2 = parseInt(prompt('ingrese el segundo numero para sumar'))
+
+alert('el resultado de la suma entre el numero 1 y 2 es ' + (num1 + num2))
